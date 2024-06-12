3d088e05c0cfddbb03950a67f05a13a7039be9fc
Evgenia Pak <dzheka_99@mail.ru>
2024-06-12 20:42:15 +0300

    Первое задание выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,14 +2,12 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  let discriminant = Math.pow(b) - 4 * a * c;
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
   if (discriminant === 0) {
-    arr = [{x1: - b / (2 * a)}];
+    arr.push(- b / (2 * a));
   } else if (discriminant > 0) {
-    arr = [{
-      x1: (- b + Math.sqrt(discriminant)) / (2 * a),
-      x2: (- b - Math.sqrt(discriminant)) / (2 * a)
-    }];
+    arr.push((- b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((- b - Math.sqrt(discriminant)) / (2 * a));
   }
   return arr;
 }
